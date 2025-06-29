0b5b38cd52638eccc236d6e8e8e2efe67618d16f
benedek fazekas <benedekfazekas@amonethir.home>
2025-06-29 13:38:23 +0100

    json exporter implementation with option edn output

--- your_cli_tool/json_exporter.py
+++ your_cli_tool/json_exporter.py
@@ -1,5 +1,6 @@
 import chess
 import typing
+import json
 from chess.pgn import BaseVisitor, SKIP, SkipType
 from typing import Optional, List, Union, Callable, Any
 
@@ -10,103 +11,107 @@ else:
     def override(fn: F, /) -> F:
         return fn
 
+def _standardize_comments(comment: Union[str, list[str]]) -> List[str]:
+    # This function ensures comments are always a list of strings
+    if isinstance(comment, str):
+        return [comment]
+    elif isinstance(comment, list):
+        return comment
+    return []
+
+def to_edn(obj):
+    """Recursively convert Python data structures to EDN strings."""
+    if isinstance(obj, dict):
+        return '{' + ' '.join(f':{k.replace("_", "-")} {to_edn(v)}' for k, v in obj.items()) + '}'
+    elif isinstance(obj, list):
+        return '[' + ' '.join(to_edn(x) for x in obj) + ']'
+    elif isinstance(obj, str):
+        return f'"{obj}"'
+    elif isinstance(obj, bool):
+        return 'true' if obj else 'false'
+    elif obj is None:
+        return 'nil'
+    else:
+        return str(obj)
+
 class JsonExporter(BaseVisitor[str]):
-    def __init__(self, *, columns: Optional[int] = 80, headers: bool = True, comments: bool = True, variations: bool = True):
-        self.columns = columns
-        self.headers = headers
-        self.comments = comments
-        self.variations = variations
+    def __init__(self, *, headers: bool = True, comments: bool = True, variations: bool = True, edn: bool
+                 = False):
+        self.headers_flag = headers
+        self.comments_flag = comments
+        self.variations_flag = variations
+        self.edn_flag = edn
 
-        self.found_headers = False
+        self.reset_game()
 
-        self.force_movenumber = True
-
-        self.lines: List[str] = []
-        self.current_line = ""
+    def reset_game(self):
+        self.game_data = {
+            "headers": {},
+            "moves": [],
+            "result": None,
+        }
+        self.current_variation = self.game_data["moves"]
+        self.variation_stack = []
         self.variation_depth = 0
 
-    def flush_current_line(self) -> None:
-        if self.current_line:
-            self.lines.append(self.current_line.rstrip())
-        self.current_line = ""
-
-    def write_token(self, token: str) -> None:
-        if self.columns is not None and self.columns - len(self.current_line) < len(token):
-            self.flush_current_line()
-        self.current_line += token
-
-    def write_line(self, line: str = "") -> None:
-        self.flush_current_line()
-        self.lines.append(line.rstrip())
-
-    def end_game(self) -> None:
-        self.write_line()
-
     def begin_headers(self) -> None:
-        self.found_headers = False
+        self.game_data["headers"] = {}
 
     def visit_header(self, tagname: str, tagvalue: str) -> None:
-        if self.headers:
-            self.found_headers = True
-            self.write_line(f"[{tagname} \"{tagvalue}\"]")
+        if self.headers_flag:
+            self.game_data["headers"][tagname] = tagvalue
 
     def end_headers(self) -> None:
-        if self.found_headers:
-            self.write_line()
+        pass  # Nothing needed here
 
     def begin_variation(self) -> Optional[SkipType]:
         self.variation_depth += 1
-
-        if self.variations:
-            self.write_token("( ")
-            self.force_movenumber = True
-            return None
-        else:
+        if not self.variations_flag:
             return SKIP
+        variation = []
+        move_entry = {"variation": variation}
+        self.current_variation.append(move_entry)
+        self.variation_stack.append(self.current_variation)
+        self.current_variation = variation
 
     def end_variation(self) -> None:
         self.variation_depth -= 1
-
-        if self.variations:
-            self.write_token(") ")
-            self.force_movenumber = True
+        if self.variation_stack:
+            self.current_variation = self.variation_stack.pop()
 
     def visit_comment(self, comment: Union[str, list[str]]) -> None:
-        if self.comments and (self.variations or not self.variation_depth):
-            def pgn_format(comments: list[str]) -> str:
-                edit = map(lambda s: s.replace("{", "").replace("}", ""), comments)
-                return " ".join(f"{{ {comment} }}" for comment in edit if comment)
-
+        if self.comments_flag:
             comments = _standardize_comments(comment)
-            self.write_token(pgn_format(comments) + " ")
-            self.force_movenumber = True
+            if self.current_variation:
+                if "comments" not in self.current_variation[-1]:
+                    self.current_variation[-1]["comments"] = []
+                self.current_variation[-1]["comments"].extend(comments)
 
     def visit_nag(self, nag: int) -> None:
-        if self.comments and (self.variations or not self.variation_depth):
-            self.write_token("$" + str(nag) + " ")
+        if self.comments_flag:
+            if self.current_variation:
+                if "nags" not in self.current_variation[-1]:
+                    self.current_variation[-1]["nags"] = []
+                self.current_variation[-1]["nags"].append(nag)
 
     def visit_move(self, board: chess.Board, move: chess.Move) -> None:
-        if self.variations or not self.variation_depth:
-            # Write the move number.
-            if board.turn == chess.WHITE:
-                self.write_token(str(board.fullmove_number) + ". ")
-            elif self.force_movenumber:
-                self.write_token(str(board.fullmove_number) + "... ")
-
-            # Write the SAN.
-            self.write_token(board.san(move) + " ")
-
-            self.force_movenumber = False
+        move_entry = {
+            "move_number": board.fullmove_number,
+            "turn": "white" if board.turn == chess.WHITE else "black",
+            "san": board.san(move),
+            "uci": move.uci(),
+        }
+        self.current_variation.append(move_entry)
 
     def visit_result(self, result: str) -> None:
-        self.write_token(result + " ")
+        self.game_data["result"] = result
 
     @override
     def result(self) -> str:
-        if self.current_line:
-            return "\n".join(itertools.chain(self.lines, [self.current_line.rstrip()])).rstrip()
+        if self.edn_flag:
+            return to_edn(self.game_data)
         else:
-            return "\n".join(self.lines).rstrip()
+            return json.dumps(self.game_data, indent=2)
 
     def __str__(self) -> str:
         return self.result()
